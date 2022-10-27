c3f7e8ee50a9acda4c3fa25ebb449a68eb034f59
Matthew Brett <matthew.brett@gmail.com>
2022-10-27 10:11:53 +0100

    Update from template

---
+++ tests/q2_1.py
@@ -0,0 +1,26 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          # rng = np.random.default_rng()
+          # trials = rng.integers(1, 11, size=(10000, 1000))
+          # scores = np.sum(trials, axis=1)
+          # np.min(scores), np.max(scores)
+          'code': r"""
+          >>> 5090 <= total_score <= 5900
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
